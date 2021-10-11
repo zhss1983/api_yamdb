7e3f6c6848d4ceff5b7866ac002abec8952fc38c
SERGEY ZHAROV <zhss.83@yandex.ru>
2021-10-12 03:48:58 +0500

    Постарался привести основную ветку в нормальный вид.

---
+++ api_yamdb/api_yamdb/models/__init__.py
@@ -0,0 +1,17 @@
+from .category import Category
+from .comment import Comment
+from .genre import Genre
+from .genre_title import Genre_Title
+from .review import Review
+from .titles import Titles
+from .user import User
+
+__all__ = [
+    'Category',
+    'Comment',
+    'Genre',
+    'Genre_Title',
+    'Review',
+    'Titles',
+    'User',
+]
