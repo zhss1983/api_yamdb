72001c92ad5ac14a7080ddb73e87ce083f6c20b7
Ivan Osyanin <madrider3@yandex.ru>
2021-10-13 13:11:47 +0300

    passowrd saving in admin fixed

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,25 +1,10 @@
-"""YaMDb URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from django.views.generic import TemplateView
 
 from rest_framework.routers import DefaultRouter
-
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .views import CommentViewSetAuthor, ReviewViewSet
 
@@ -43,4 +28,7 @@ urlpatterns = [
         name='redoc'
     ),
     path('api/v1/', include(api_router_v1.urls)),
+    # Getting token pair for user
+    path('api/v1/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+
 ]
