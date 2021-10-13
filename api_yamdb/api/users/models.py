73bbbc89e6357824b4634291681746db5d7f8d02
Ivan Osyanin <madrider3@yandex.ru>
2021-10-13 23:21:27 +0300

    default admin for superuser added

--- api_yamdb/api/users/models.py
+++ api_yamdb/api/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from .managers import CustomUserManager
 
 class User(AbstractUser):
     ACCESS_LEVEL = (
@@ -21,6 +22,8 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
 
     REQUIRED_FIELDS = ['email']
+
+    objects = CustomUserManager()
 
     class Meta:
         verbose_name = 'Пользователь'
