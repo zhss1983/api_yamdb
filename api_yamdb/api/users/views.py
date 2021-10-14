f5b1d500f10f39e9317c4e3d1caa8614b077e46d
Ivan Osyanin <madrider3@yandex.ru>
2021-10-15 01:39:32 +0300

    Функционал USERS завершен на 90%

--- api_yamdb/api/users/views.py
+++ api_yamdb/api/users/views.py
@@ -1,9 +1,15 @@
-from rest_framework import filters, viewsets
+from django.shortcuts import get_object_or_404
 
+from rest_framework import filters, viewsets
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from api.users.models import User
 from .permissions import IsAdmin
-from .serializers import UserSerializer
+from .serializers import MyTokenObtainPairSerializer, UserSerializer
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -13,3 +19,26 @@ class UserViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields = ('=username',)
     lookup_field = 'username'
+
+    def get_object(self):
+        queryset = self.get_queryset()
+        obj = get_object_or_404(queryset)
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+    def get_queryset(self):
+        if self.kwargs['username'] == 'me':
+            queryset = User.objects.filter(id=self.request.user.id)
+            return queryset
+        return User.objects.all()
+
+    # Прописать разрешения прав для не администраторов
+
+    # def get_permissions(self):
+    #     if (self.action == 'retrieve' and
+    #             self.kwargs['username'] == 'me'):
+    #         print('retrieve', self.kwargs)
+    #     if (self.action == 'partial_update' and
+    #             self.kwargs['username'] == 'me'):
+    #         print('partial_update', self.kwargs)
+    #     return super().get_permissions()
