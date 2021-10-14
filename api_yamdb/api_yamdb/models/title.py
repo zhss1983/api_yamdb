630116edbc4dc1eaf11c8d3a0afa621849d1ee51
Asmodai <asmoday.p@gmail.com>
2021-10-14 09:44:29 +0300

    Добавил вьюсет для произведений, пофиксил Titles - Title в некоторых файлах

--- api_yamdb/api_yamdb/models/title.py
+++ api_yamdb/api_yamdb/models/title.py
@@ -1,4 +1,7 @@
+import datetime as dt
+
 from django.db import models
+from django.db.models.functions import Now
 
 from .category import Category
 from .genre import Genre
@@ -19,3 +22,11 @@ class Title(models.Model):
         related_name='category',
     )
     description = models.TextField('Описание')
+
+    class Meta:
+        constraints = (
+            models.CheckConstraint(
+                check=models.Q(year__gt=Now()),
+                name="year_cannot_be_bigger_then_current"
+            ),
+        )
