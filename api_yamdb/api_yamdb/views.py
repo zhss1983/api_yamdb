08fc7cdc413bddc46fa3796112adf49d9ce1748d
Asmodai <asmoday.p@gmail.com>
2021-10-15 11:50:06 +0300

    Add search for Categories

--- api_yamdb/api_yamdb/views.py
+++ api_yamdb/api_yamdb/views.py
@@ -1,5 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework import filters
 from rest_framework.mixins import CreateModelMixin, ListModelMixin
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.pagination import PageNumberPagination
@@ -79,3 +79,5 @@ class CategoryViewSet(ModelViewSet):
     serializer_class = CategorySerializer
     permission_classes = (AllowAny,)
     pagination_class = LimitOffsetPagination
+    filter_backends =(filters.SearchFilter,)
+    search_fields = ('^name',)
