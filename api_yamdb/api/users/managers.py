67d2346848e827e13cf601d536e78c8c30cedceb
SERGEY ZHAROV <zhss.83@yandex.ru>
2021-10-19 22:52:36 +0500

    Второе ревью, четвёртая попытка залить на сервер.

--- api_yamdb/api/users/managers.py
+++ api_yamdb/api/users/managers.py
@@ -18,3 +18,4 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('role', ADMIN)
+        return self.create_user(email, password, **extra_fields)
